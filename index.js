226661188c281dc72e66d0d5915fc7bb424d8426
spandexVortex123 <spandexVortex123@protonmail.com>
2022-08-13 10:45:45 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParse = require('body-parser')
+
+// Middlewares
+app.use(bodyParse.json())
+
+app.get('/', (req, res) => {
+	res.json({
+		message: "hello world"
+	})
+})
+
+app.post('/post', (req, res) => {
+	let data = req.body
+	console.log(data)
+	res.send('asdf')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
