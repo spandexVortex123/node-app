1cbe9fd34923e59a2795a7655d14324fbc4a5b4d
spandexVortex123 <spandexVortex123@protonmail.com>
2022-08-14 18:53:26 +0530

    Added JWT authentication. User input checks done.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParse = require('body-parser')
-const sequelize = require('./db')
+const jwt = require('jsonwebtoken')
+const md5 = require('md5')
+const secret = 'jwtsecret'
 
 const Note = require('./model/note')
 const User = require('./model/user')
@@ -23,29 +25,71 @@ app.get('/', (req, res) => {
 	})()
 })
 
-app.post('/post', (req, res) => {
-	let data = req.body
-	console.log(data)
-	res.send('asdf')
+app.post('/login', (req, res) => {
+
+	if (!req.body.username || !req.body.password) {
+		res.json({
+			message: 'Username/Password cannot be empty'
+		})
+	} else {
+		let username = req.body.username
+		let password = req.body.password
+
+		let passwordHash = md5(password);
+
+		(async function () {
+			let user = await User.findAll({
+				where: {
+					username,
+					password: passwordHash
+				}
+			})
+
+			if (!user[0]) {
+				res.json({
+					message: 'Username/Password incorrect'
+				})
+			} else {
+				let { id, password, createdAt, updatedAt, ...data } = user[0].dataValues;
+				// console.log(data)
+				let token = jwt.sign(data, secret, {
+					expiresIn: '300s'
+				})
+				res.json({
+					message: 'Login successful',
+					token
+				})
+			}
+
+		})()
+		// res.send('asdf')
+	}
 })
 
 app.post('/register', (req, res) => {
-	let username = req.body.username
-	let password = req.body.password
 
-	try {
-		(async function () {
-			let user = User.build({ username, password })
-			var savedUser = await user.save()
+	if (!req.body.username && !req.body.password) {
+		res.json({
+			message: 'Username/Password cannot be empty'
+		})
+	} else {
+		let username = req.body.username
+		let password = md5(req.body.password)
 
-			res.json({
-				message: 'User Registered successfully',
-				data: savedUser
-			})
-		})()
+		try {
+			(async function () {
+				let user = User.build({ username, password })
+				var savedUser = await user.save()
 
-	} catch (error) {
+				res.json({
+					message: 'User Registered successfully',
+					data: savedUser
+				})
+			})()
 
+		} catch (error) {
+
+		}
 	}
 
 })
@@ -61,48 +105,83 @@ app.get('/notes', (req, res) => {
 })
 
 app.post('/addNote', (req, res) => {
-	// console.log(req.query["username"])
-	let username = req.query.username;
-	(async function () {
-		let user = await User.findAll({
-			where: {
-				username: username
-			}
-		})
-		// console.log(user[0].id)
-		let note = Note.build({ description: req.body.description })
-		let savedNote = await note.save()
-		// console.log(savedNote)
-		savedNote.update({
-			userId: user[0].id
-		})
-		res.send(user)
 
-	})()
+	if (req.headers.authorization) {
+
+		let token = req.headers.authorization.split(' ')[1]
+
+		try {
+			let data = jwt.verify(token, secret);
+			// console.log(data)
+			if (data.username === req.query.username) {
+				// console.log(req.query["username"])
+				let username = req.query.username;
+				(async function () {
+					let user = await User.findAll({
+						where: {
+							username: username
+						}
+					})
+					// console.log(user[0].id)
+					let note = Note.build({ description: req.body.description })
+					let savedNote = await note.save()
+					// console.log(savedNote)
+					savedNote.update({
+						userId: user[0].id
+					})
+					res.json({
+						message: 'Note added successfully'
+					})
+				})()
+			} else {
+				res.json({
+					message: 'Username incorrect'
+				})
+			}
+		} catch (error) {
+			res.json({
+				message: error.message
+			})
+		}
+	} else {
+		res.json({
+			message: 'Token not present in header'
+		})
+	}
 })
 
 app.get('/usernotes', (req, res) => {
-	let username = req.query.username;
-	(async function () {
-		let user = await User.findAll({
-			where: {
-				username
-			}
-		})
 
-		let notesList = await Note.findAll({
-			where: {
-				userId: user[0].id
-			}
-		})
-
+	if (!req.headers.authorization) {
 		res.json({
-			message: `Notes list for user ${username}`,
-			data: notesList
+			message: 'Token not present'
 		})
-
-	})()
+	} else {
+		let token = req.headers.authorization.split(' ')[1];
+		try {
+			let data = jwt.verify(token, secret);
+			(async function () {
+				let user = await User.findAll({
+					where: {
+						username: data.username
+					}
+				})
+				let notesList = await Note.findAll({
+					where: {
+						userId: user[0].id
+					}
+				})
+				res.json({
+					message: `Notes list for user ${data.username}`,
+					data: notesList
+				})
+			})()
+		} catch (error) {
+			res.json({
+				message: error.message
+			})
+		}
+	}
 })
-
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
